ba1f6f39ecccbcac64a1d2aa6444823b408adf09
Amer Fahmy <afahmy519@gmail.com>
2020-04-28 19:10:57 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,27 @@
+def using_concat(my_favorite_things, favs)
+  my_favorite_things.concat (favs)
+end
+  
+  
+def using_insert(array, love)
+  
+  array.insert(4, love)
+end
+
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten 
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, integer)
+  array.delete_at(2)
+  
+end
